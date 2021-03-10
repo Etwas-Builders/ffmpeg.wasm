ee1a9493272f290515c5c01d5c34b979fb9c5903
Rahul Tarak <cryogenicplanet@gmail.com>
2021-03-10 14:45:46 +0530

    v0.1.2-f

--- src/browser/getModule.js
+++ src/browser/getModule.js
@@ -1,36 +1,46 @@
+const { default: Bugsnag } = require('@bugsnag/js');
 const { log } = require('../utils/log');
 
 module.exports = async ({ corePath }) => {
-  if (typeof window.Module === 'undefined') {
-    log('info', 'fetch ffmpeg-core.worker.js script');
-    const workerBlob = await (
-      await fetch(corePath.replace('ffmpeg-core.js', 'ffmpeg-core.worker.js'))
-    ).blob();
-    window.FFMPEG_CORE_WORKER_SCRIPT = URL.createObjectURL(workerBlob);
-    log('info', `worker object URL=${window.FFMPEG_CORE_WORKER_SCRIPT}`);
-    log('info', `download ffmpeg-core script (~25 MB) from ${corePath}`);
-    const timeoutPromise = new Promise((_, reject) => {
-      setTimeout(() => {
-        reject(new Error('Loading FFmpeg Failed!'));
-      }, 60000);
-    });
-    const loadFFmpeg = new Promise((resolve) => {
-      const script = document.createElement('script');
-      const eventHandler = () => {
-        script.removeEventListener('load', eventHandler);
-        log('info', 'initialize ffmpeg-core');
-        window.Module.onRuntimeInitialized = () => {
-          log('info', 'ffmpeg-core initialized');
-          resolve(window.Module);
+  try {
+    if (typeof window.Module === 'undefined') {
+      log('info', 'fetch ffmpeg-core.worker.js script');
+      const workerBlob = await (
+        await fetch(corePath.replace('ffmpeg-core.js', 'ffmpeg-core.worker.js'))
+      ).blob();
+      window.FFMPEG_CORE_WORKER_SCRIPT = URL.createObjectURL(workerBlob);
+      log('info', `worker object URL=${window.FFMPEG_CORE_WORKER_SCRIPT}`);
+      log('info', `download ffmpeg-core script (~25 MB) from ${corePath}`);
+      const timeoutPromise = new Promise((_, reject) => {
+        setTimeout(() => {
+          reject(new Error('Loading FFmpeg Failed!'));
+        }, 60000);
+      });
+      const loadFFmpeg = new Promise((resolve) => {
+        const script = document.createElement('script');
+        const eventHandler = () => {
+          script.removeEventListener('load', eventHandler);
+          log('info', 'initialize ffmpeg-core');
+          window.Module.onRuntimeInitialized = () => {
+            log('info', 'ffmpeg-core initialized');
+            resolve(window.Module);
+          };
         };
-      };
-      script.src = corePath;
-      script.type = 'text/javascript';
-      script.addEventListener('load', eventHandler);
-      document.getElementsByTagName('head')[0].appendChild(script);
+        script.src = corePath;
+        script.type = 'text/javascript';
+        script.addEventListener('load', eventHandler);
+        document.getElementsByTagName('head')[0].appendChild(script);
+      });
+      return Promise.race([loadFFmpeg, timeoutPromise]);
+    }
+    log('info', 'ffmpeg-core is loaded already');
+    return Promise.resolve(window.Module);
+  } catch (err) {
+    Bugsnag.notify(err, (event) => {
+      event.addMetadata('loadingFFmpeg', {
+        corePath,
+      });
     });
-    return Promise.race([loadFFmpeg, timeoutPromise]);
+    throw new Error(err);
   }
-  log('info', 'ffmpeg-core is loaded already');
-  return Promise.resolve(window.Module);
 };
